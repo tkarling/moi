47ae347f71acc1d3e8fa15fb4398d923970b2743
tkarling <tkarling@live.com>
2015-07-31 14:39:02 -0600

    auth for rtfm

--- rtfmApp/js/app.js
+++ rtfmApp/js/app.js
@@ -5,11 +5,43 @@ angular.module("myApp")
     .constant("FB", {
         url: "https://blinding-heat-8535.firebaseio.com/rtfmApp"
     })
+    .run(["$rootScope", "$location", function($rootScope, $location) {
+        $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+            // We can catch the error thrown when the $requireAuth promise is rejected
+            // and redirect the user back to the home page
+            console.log("hello");
+            if (error === "AUTH_REQUIRED") {
+                $location.path("/login");
+            }
+        });
+    }])
     .config(function($routeProvider) {
         $routeProvider
+            .when('/login', {
+                templateUrl: "./js/auth/auth.html",
+                controller: "AuthController",
+                resolve: {
+                    // controller will not be loaded until $waitForAuth resolves
+                    // Auth refers to our $firebaseAuth wrapper in the example above
+                    "currentAuth": function(Auth) {
+                        // $waitForAuth returns a promise so the resolve waits for it to complete
+                        return Auth.$waitForAuth();
+                    }
+
+                }
+            })
             .when('/threads', {
                 templateUrl: "./js/threads/threads.html",
-                controller: "ThreadsController"
+                controller: "ThreadsController",
+                resolve: {
+                    // controller will not be loaded until $requireAuth resolves
+                    // Auth refers to our $firebaseAuth wrapper in the example above
+                    "currentAuth": ["Auth", function(Auth) {
+                        // $requireAuth returns a promise so the resolve waits for it to complete
+                        // If the promise is rejected, it will throw a $stateChangeError (see above)
+                        return Auth.$requireAuth();
+                    }]
+                }
 
             })
             .when('/threads/:threadId', {
@@ -20,11 +52,18 @@ angular.module("myApp")
                         return threadsService.getThread($route.current.params.threadId);
                     },
                     comments: function(threadsService, $route) {
-                        return threadsService.getComments($route.current.params.threadId);
-                    }
+                            return threadsService.getComments($route.current.params.threadId);
+                        },
+                        // controller will not be loaded until $requireAuth resolves
+                        // Auth refers to our $firebaseAuth wrapper in the example above
+                    "currentAuth": ["Auth", function(Auth) {
+                        // $requireAuth returns a promise so the resolve waits for it to complete
+                        // If the promise is rejected, it will throw a $stateChangeError (see above)
+                        return Auth.$requireAuth();
+                    }]
                 }
             })
             .otherwise({
-                redirectTo: '/threads'
+                redirectTo: '/login'
             })
     });
---
+++ rtfmApp/js/auth/authCtrl.js
@@ -0,0 +1,77 @@
+angular.module("myApp")
+    .factory("Auth", ["$firebaseAuth", "FB", 
+        function($firebaseAuth, FB) {
+            var ref = new Firebase(FB.url);
+            return $firebaseAuth(ref);
+        }
+    ])
+    .controller("AuthController", function($scope, $location, $firebaseAuth, $firebaseObject, FB) {
+        var firebaseRoot = FB.url,
+            ref = new Firebase(firebaseRoot),
+            authObj = $firebaseAuth(ref);
+        // Assign authObj to scope for inline calls in the view
+        $scope.authObj = authObj;
+        // Use $onAuth listener to keep $scope.authData synced. Also sync scope.user.
+        authObj.$onAuth(function(authData) {
+            $scope.authData = authData;
+            if (authData) { // Set up user object if authData present
+                var userRef = new Firebase(firebaseRoot + '/users/' + authData.uid),
+                    user = $firebaseObject(userRef);
+                user.$loaded().then(function(user) { // Wait for user to be loaded before setting user details
+                    user.lastLogin = new Date().toString();
+                    user.$save();
+                    user.$bindTo($scope, 'user');
+                    if (user.birthday) {
+                        $scope.birthday = new Date(user.birthday);
+                    }
+                    $location.path("/threads");
+                });
+            }
+        });
+        $scope.login = function(user) {
+            authObj.$authWithPassword(user).then(function(authData) {
+                console.log('login authData', authData);
+            }, function(error) {
+                alert(error);
+            });
+        };
+        $scope.register = function(user) {
+            authObj.$createUser(user).then(function(userData) {
+                console.log('login userData', userData);
+                $scope.login(user);
+            }, function(error) {
+                alert(error);
+            });
+        };
+        $scope.reset = function(user) {
+            authObj.$resetPassword({
+                email: user.email
+            }).then(function() {
+                alert('Success! Password reset email sent.');
+            }, function(error) {
+                alert('Error! ' + JSON.stringify(error));
+            });
+        };
+        $scope.changePassword = function(email, oldPassword, newPassword, newPasswordConfirmed) {
+            if (newPassword !== newPasswordConfirmed) {
+                alert('Passwords do not match!');
+            } else {
+                authObj.$changePassword({
+                    email: email,
+                    oldPassword: oldPassword,
+                    newPassword: newPassword
+                }).then(function() {
+                    alert('success!');
+                }, function(error) {
+                    alert('Failure! ' + JSON.stringify(error));
+                });
+            }
+        };
+        $scope.googlePlus = function() {
+            authObj.$authWithOAuthPopup('google');
+        };
+        $scope.logout = function() {
+        	authObj.$unauth();
+        	$location.path("/login");
+        }
+    });
